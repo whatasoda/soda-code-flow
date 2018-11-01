ca22c831e8b25a723fcc872dcdefd749817babe4
whatasoda <a16dc107@dhw.ac.jp>
2018-11-02 04:00:59 +0900

    move styles to app/asset

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './app';
-import './common.css';
+import './asset/common.css';
 import fetchCodeFlow from './effects/codeFlow/fetchCodeFlow';
 
 ReactDOM.render(<App />, document.getElementById('app'));
